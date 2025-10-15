f667e8ed110edfa7151d53350433cd5198632f46
ITDdev17 <itddev17.intonly@hld.com>
2025-10-15 09:55:16 +0800

    Update core package to 1.0.3 and publish to npm

--- packages/example/src/main.tsx
+++ packages/example/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-// import '@lemon-design-xx/core/dist/index.css'
+import '@lemon-design-xx/core/dist/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
